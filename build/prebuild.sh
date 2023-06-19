158ca288e6461494880efc15832ebbf7066a8ae8
ShaoboFeng <fengshaobo@huawei.com>
2023-06-19 11:55:45 +0800

    add meson deps

--- build/prebuild.sh
+++ build/prebuild.sh
@@ -102,6 +102,7 @@ cd ${PROJECT_DIR}
 if [ ! -d ${PROJECT_DIR}/prebuilts/rpm/ft_surface_wrapper ]; then
 git clone https://gitee.com/ShaoboFeng/mesa-fangtian.git -b ft_dev ${PROJECT_DIR}/prebuilts/rpm/mesa-fangtian
 fi
+export PATH=prebuilts/build-tools/linux-x64/bin:$PATH
 cd ${PROJECT_DIR}/prebuilts/rpm/mesa-fangtian
 ./build.sh
 cd mesa-openEuler-22.03-LTS/mesa-21.3.1/build
