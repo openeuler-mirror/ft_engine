4da6369fd00435e423ca8711207c0957a37b1786
abc12133 <jinguoen@huawei.com>
2023-06-20 15:16:54 +0800

    compile libwmutil
    Signed-off-by: abc12133 <jinguoen@huawei.com>

---
+++ window_manager/ft_adapter/bundle_constants.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef BUNDLE_CONSTANTS_H
+#define BUNDLE_CONSTANTS_H
+
+namespace OHOS {
+namespace AppExecFwk {
+namespace Constants {
+constexpr int PERMISSION_GRANTED = 0;
+}  // namespace Constants
+}  // namespace AppExecFwk
+}  // namespace OHOS
+#endif  // BUNDLE_CONSTANTS_H
