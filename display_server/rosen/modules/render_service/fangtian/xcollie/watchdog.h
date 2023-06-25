f63ab3f65e6914de3a90b45d62f3561368c15063
yanansong <songyanan5@huawei.com>
2023-06-25 11:30:45 +0800

    del useless code

--- display_server/rosen/modules/render_service/fangtian/xcollie/watchdog.h
+++ display_server/rosen/modules/render_service/fangtian/xcollie/watchdog.h
@@ -28,58 +28,9 @@ public:
         return ins;
     }
 
-    // /**
-    //  * Add handler to watchdog thread with customized check interval
-    //  *
-    //  * @param name, the name of handler check task
-    //  * @param handler, the handler to be checked periodically
-    //  * @param timeOutCallback, callback when timeout
-    //  * @param interval, the period in millisecond
-    //  * @return 0 if added
-    //  *
-    //  */
-    // int AddThread(const std::string &name, std::shared_ptr<AppExecFwk::EventHandler> handler,
-    //     TimeOutCallback timeOutCallback = nullptr, uint64_t interval = WATCHDOG_TIMEVAL);
-
-    /**
-     * Add handler to watchdog thread with customized check interval
-     *
-     * @param name, the name of handler check task
-     * @param handler, the handler to be checked periodically
-     * @param interval, the period in millisecond
-     * @return 0 if added
-     *
-     */
     int AddThread(const std::string &name, std::shared_ptr<AppExecFwk::EventHandler> handler) {
         return 0;
     }
-
-    // /**
-    //  * Run a onshot task in shared watchdog thread, the submitted task should never be time consuming
-    //  *
-    //  * @param name, task name
-    //  * @param task, a short functiona
-    //  * @param delay, delay a few millisecond to run the task
-    //  *
-    //  */
-    // void RunOneShotTask(const std::string& name, Task&& task, uint64_t delay = 0);
-
-    // /**
-    //  * Run a periodical task in shared watchdog thread
-    //  *
-    //  * @param name, task name
-    //  * @param task, a short functiona
-    //  * @param interval, the millisecond interval of the periodical task
-    //  * @param delay, delay a few millisecond to first run the task
-    //  *
-    //  */
-    // void RunPeriodicalTask(const std::string& name, Task&& task, uint64_t interval, uint64_t delay = 0);
-
-    // /**
-    //  * stop watchdog thread and wait for cleanup synchronously
-    //  *
-    //  */
-    // void StopWatchdog();
 };
 } // end of namespace HiviewDFX
 } // end of namespace OHOS
