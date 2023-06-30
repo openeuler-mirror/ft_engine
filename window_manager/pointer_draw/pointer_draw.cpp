e863a67b3f58ed29e7620ea00110ccbbbdf31519
abc12133 <jinguoen@huawei.com>
2023-06-30 10:16:01 +0800

    remove useless code
    Signed-off-by: abc12133 <jinguoen@huawei.com>

--- window_manager/pointer_draw/pointer_draw.cpp
+++ window_manager/pointer_draw/pointer_draw.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2023 Huawei Technologies Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -12,10 +12,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include "pointer_draw.h"
 
-#include "unistd.h"
-#include <sys/stat.h>
+#include "pointer_draw.h"
 
 #include "window_manager_hilog.h"
 #include "display_manager_service_inner.h"
