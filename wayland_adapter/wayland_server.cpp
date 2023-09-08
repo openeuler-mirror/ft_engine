d04b6e0c71d90b9dc174344f4d249687522acf6f
yanansong <songyanan5@huawei.com>
2023-09-08 17:37:40 +0800

    add wl_output

--- wayland_adapter/wayland_server.cpp
+++ wayland_adapter/wayland_server.cpp
@@ -38,7 +38,7 @@ void WaylandServer::CreateGlobalObjects()
 {
     compositorGlobal_ = WaylandCompositor::Create(display_);
     xdgWmBaseGlobal_ = WaylandXdgWmBase::Create(display_);
-
+    outputGlobal_ = WaylandOutput::Create(display_);
     wl_display_add_shm_format(display_, WL_SHM_FORMAT_RGBA8888);
     wl_display_init_shm(display_);
 }
